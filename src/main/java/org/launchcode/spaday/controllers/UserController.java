d743cfda0fbf95f0e010300557edb60e040fb4c6
Ben <bensztukowski@gmail.com>
2021-05-28 11:49:54 -0500

    Studio in progress at step 5

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,28 @@
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    public String displayAddUserForm(){
+        return "user/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify){
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        if (verify.equals(user.getPassword())){
+            model.addAttribute("password", user.getPassword());
+            return "user/index";
+        } else {
+            return "user/add";
+        }
+    }
+}
